Reversed direction of rotation to match with the angle calculations. calculateResult() now spits out the correct answer. Still not drawing text on lighter fields

Model/GUIController.py
@@ -55,7 +55,6 @@ class GUIController:
         self.angleRotated = 0
         self.targetAngle = np.random.randint(360, 5 * 360)
         self.rotateWheel()
-        # TODO: Pop-up with result
 
     def rotateWheel(self):
         if self.angleRotated > self.targetAngle:
@@ -75,6 +74,7 @@ class GUIController:
             self.drawArrow()
 
         else:
+            print(self.angle)
             self.calculateResult()
 
     def resetWheel(self):
@@ -99,6 +99,7 @@ class GUIController:
 
     def calculateResult(self):
         anglePerSection = 360 / len(self.wheelData)
-        sectionIndex = int(self.angle / anglePerSection) % len(self.wheelData)
-        selectedField = self.wheelData[sectionIndex]
+        adjustedAngle = (self.angle + 90) % 360
+        sectionIndex = (int(adjustedAngle / anglePerSection)) % len(self.wheelData)
+        selectedField = self.wheelData[-sectionIndex - 1]
         print("Landed on field: ", selectedField)

Model/Wheel.py
@@ -29,8 +29,8 @@ class Wheel:
         self.canvas.create_oval(x0, y0, x1, y1, fill="white")
 
         for i, description in enumerate(self.descriptions):
-            startAngle = anglePerSection * i + self.angle
-            txtStartAngle = anglePerSection * i - self.angle
+            startAngle = anglePerSection * i - self.angle
+            txtStartAngle = anglePerSection * i + self.angle
             self.canvas.create_arc(x0, y0, x1, y1,
                                    start=startAngle,
                                    extent=anglePerSection,
@@ -40,12 +40,10 @@ class Wheel:
             angle_rad = np.radians(midpointAngle)
             textX = centerX + radius * np.cos(angle_rad)
             textY = centerY + radius * np.sin(angle_rad)
-            self.canvas.create_text(textX, textY, text=description, font=("Arial", 14), fill="white",
+            self.canvas.create_text(textX, textY, text=description, font=("Arial", 14), fill=self.getTextColor(i),
                                     angle=anglePerSection * i + startAngle)
 
         self.canvas.create_oval(centerX - 85, centerY - 85, centerX + 85, centerY + 85, fill="white")
-
-
 
     def getColorBrightness(self, color, sectionNr):
         rgb = self.tkColorToRgb(color)
@@ -80,3 +78,9 @@ class Wheel:
         minimumIntensity = 10
         stepChange = (100 - minimumIntensity) / (self.sections - 1)
         return 100 - stepChange * sectionNr
+
+    def getTextColor(self, sectionNr):
+        if sectionNr < self.sections / 2:
+            return "black"
+        else:
+            return "white"