c058671f9f74e7567ac23a4884fc87c3c6996baf
Anton <boblo.ludvigsen@gmail.com>
2024-01-27 06:44:44 +0100

    Changed the geometry handling to grid to include a results page. The results are done. Handling of the completion of character sucks

--- Model/GUIController.py
+++ Model/GUIController.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import END
 
 import numpy as np
 
@@ -26,6 +27,7 @@ class GUIController:
         The brains of the operation
         :param root: The tk.Tk() root object
         """
+        self.resultTextbox = None
         self.spinTypeLabel = None
         self.colors = ["red", "blue", "green", "yellow", "purple", "orange", "pink"]
         self.spinCount = 0
@@ -45,19 +47,26 @@ class GUIController:
 
     def setupWidgets(self):
         label = tk.Label(self.root, text="Let the wheel decide your destiny!", font=('Arial', 18))
-        label.pack(padx=20, pady=20)
+        label.grid(row=0, column=0, pady=10)
+
         self.spinTypeLabel = tk.Label(self.root, text=self.getSpinType(), font=('Arial', 14))
-        self.spinTypeLabel.pack()
+        self.spinTypeLabel.grid(row=1, column=0, pady=5)
         self.canvas = tk.Canvas(self.root, width=600, height=600)
-        self.canvas.pack()
+        self.canvas.grid(row=2, column=0, padx=10, pady=10)
 
         self.wheelData = getWheelData(self.getSpinType())
         self.color = self.colors[np.random.randint(len(self.colors))]
         self.wheel = Wheel(self.canvas, self.wheelData, self.root, self.angle, self.color)
 
         self.spinButton = tk.Button(self.root, text="Spin", command=self.spinWheel)
-        self.spinButton.pack(padx=20, pady=20)
+        self.spinButton.grid(row=3, column=0, padx=20, pady=20)
         self.drawArrow()
+
+        resultLabel = tk.Label(self.root, text="Results", font=('Arial', 16))
+        resultLabel.grid(row=1, column=1)
+        self.resultTextbox = tk.Text(self.root, bg="white", width=40, height=33, padx=10)
+        self.resultTextbox.grid(row=2, column=1, padx=10, pady=10)
+        self.setupResults()
 
     def spinWheel(self):
         if not self.spinCount == 0:
@@ -116,6 +125,7 @@ class GUIController:
         selectedField = self.wheelData[sectionIndex]
         spinType = self.getSpinType()
         print(spinType, ":", selectedField)
+        self.addResult(selectedField)
         self.spinCount += 1
         self.spinButton.config(state='normal')
 
@@ -123,7 +133,27 @@ class GUIController:
         for spinType in SpinTypes:
             if spinType.value == self.spinCount:
                 return spinType.name.capitalize()
-
         # TODO: Make pop-up to show that the character is done
-        # TODO: Show the final stats and make them easy to copy
+        # FIXME handle the end in a place that doesn't get called from multiple places
+        self.setupResults()
         self.spinCount = 0
+
+    def setupResults(self):
+        self.resultTextbox.config(state='normal')
+        self.resultTextbox.delete('1.0', END)
+        for spinType in SpinTypes:
+            text = f"{spinType.name.replace('_', ' ').capitalize()}:\n"
+            self.resultTextbox.insert(END, text)
+        self.resultTextbox.config(state='disabled')
+
+    def addResult(self, result):
+        self.resultTextbox.config(state='normal')
+        currentText = self.resultTextbox.get('1.0', END)
+        for spinType in SpinTypes:
+            if spinType.value == self.spinCount:
+                toBeReplaced = f"{spinType.name.replace('_', ' ').capitalize()}:"
+                result = f"{toBeReplaced} {result}"
+                currentText = currentText.replace(toBeReplaced, result)
+                self.resultTextbox.delete('1.0', END)
+                self.resultTextbox.insert('1.0', currentText)
+        self.resultTextbox.config(state='disabled')
--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 from Model.CharacterCreatorApp import CharacterCreatorApp
 
-
-def main():
-    app = CharacterCreatorApp()
-
-
 if __name__ == '__main__':
-    main()
+    CharacterCreatorApp()
 
